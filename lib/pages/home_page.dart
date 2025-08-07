e7519e807977519c3f53c50466cb872207e6e415
Isaac3105 <isaac.absantos@gmail.com>
2025-08-07 13:28:34 +0100

    Started on a list widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List toDoList = [
+    ["Learn App Dev", false],
+    ["Learn Web Dev", true],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Text('Simple To Do'),
+        backgroundColor: Colors.deepPurpleAccent,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return Container(
+            child: Text(
+              toDoList[index][0]
+              ),
+            );
+        },
+      ),
     );
   }
 }
